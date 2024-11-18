5814b047db5ed89a312c9e9e60cdaf0c08bc2d62
ThiagoMachado2 <136006071+ThiagoMachado2@users.noreply.github.com>
2024-11-18 14:16:49 -0300

    teste

--- src/main/java/com/example/clinica/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/example/clinica/infrastructure/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.clinica.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,11 +13,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Desabilita CSRF, pois é comum em APIs sem estado
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        http.csrf(csrf -> csrf.disable()) // Desabilita CSRF, pois não é necessário em APIs REST
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // API sem estado
                 .authorizeRequests(authz -> authz
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Permite o acesso às rotas do Swagger
-                        .anyRequest().authenticated()); // Exige autenticação para qualquer outra requisição
+                        .requestMatchers(
+                                "/v3/api-docs/**", // Endpoints do Swagger
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/h2-console/**", // Exemplo: H2 Console, se usado
+                                "/public/**" // Adicione aqui outras rotas públicas, se necessário
+                        ).permitAll() // Permite acesso às rotas acima sem autenticação
+                        .anyRequest().permitAll()); // Permite acesso a todas as outras rotas sem autenticação
 
         return http.build();
     }
